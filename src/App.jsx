325dd03e55f0819b7c545104b1c141236eda4097
Marcus Bower <marcusbower@Marcuss-MacBook-Pro.local>
2026-04-29 09:44:28 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <text id="main-title">HELLO, WORLD!</text>
+  )
+}
+
+export default App
