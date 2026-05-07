8472dd90700d50b0a4cf34cffe342afa949f59c8
Marcus Bower <marcusbower02@gmail.com>
2026-05-07 00:29:04 -0700

    fleshed out frontend layout including logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
-import { useState } from 'react'
 import './App.css'
+import icon from './assets/SSLogo.png'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <text id="main-title">HELLO, WORLD!</text>
+    <div id="screen">
+      <header id="title-card">
+        <img src={icon} id="logo" alt='icon'></img>
+        <h1 id="main-title">Song Swap</h1>
+      </header>
+      <main id="main-body">
+        <div id="song-swap-main">
+          <div class="song-cont">
+            <h2>Send</h2>
+            <img class="album-art" alt='album_art'/>
+            <h3>Song title</h3>
+            <div class="input-group">
+            <input placeholder="Paste Spotify Link" />
+            <button>Send</button>
+            </div>
+          </div>
+          <div class="song-cont">
+            <h2>Recieve</h2>
+            <img class="album-art" alt='album_art'/>
+            <h3>Song title</h3>
+          </div>
+        </div>
+        <p>Welcome To Song Swap: Work in Progress</p>
+      </main>
+    </div>
   )
 }
-
 export default App
